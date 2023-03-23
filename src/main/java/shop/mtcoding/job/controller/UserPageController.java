a34617e82b4400b2b6929af6022e7e2243ebc161
khh5762 <khh5762@naver.com>
2023-03-23 10:55:48 +0900

    Refactor: UserPage, Resume, Bookmark Controller Rest형식에 맞춰 완료

--- src/main/java/shop/mtcoding/job/controller/UserPageController.java
+++ src/main/java/shop/mtcoding/job/controller/UserPageController.java
@@ -1,133 +1,82 @@
 package shop.mtcoding.job.controller;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.servlet.http.HttpSession;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import shop.mtcoding.job.dto.apply.ApplyRespDto.ApplyListForUserRespDto;
-import shop.mtcoding.job.dto.bookmark.BookmarkReqDto;
-import shop.mtcoding.job.dto.recruitmentPost.RecruitmentPostRespDto.RecruitmentPostListRespDto;
-import shop.mtcoding.job.dto.userSkill.UserMatchingDto;
+import lombok.RequiredArgsConstructor;
+import shop.mtcoding.job.dto.ResponseDto;
+import shop.mtcoding.job.dto.userPage.UserPageApplyDto;
+import shop.mtcoding.job.dto.userPage.UserPageBookmarkDto;
+import shop.mtcoding.job.dto.userPage.UserPageMatchingDto;
 import shop.mtcoding.job.handler.exception.CustomException;
 import shop.mtcoding.job.model.apply.ApplyRepository;
-import shop.mtcoding.job.model.applyResume.ApplyResume;
-import shop.mtcoding.job.model.applyResume.ApplyResumeRepository;
 import shop.mtcoding.job.model.bookmark.BookmarkRepository;
-import shop.mtcoding.job.model.recruitmentPost.RecruitmentPostRepository;
 import shop.mtcoding.job.model.user.User;
 import shop.mtcoding.job.model.userSkill.UserSkillRepository;
+import shop.mtcoding.job.util.Convert;
 
-@Controller
+@RequiredArgsConstructor
+@RestController
 public class UserPageController {
-    @Autowired
-    private HttpSession session;
+    private final HttpSession session;
 
-    @Autowired
-    private ApplyRepository applyRepository;
+    private final ApplyRepository applyRepository;
 
-    @Autowired
-    private ApplyResumeRepository applyResumeRepository;
+    private final UserSkillRepository userSkillRepository;
 
-    @Autowired
-    private RecruitmentPostRepository recruitmentPostRepository;
-
-    @Autowired
-    private UserSkillRepository userSkillRepository;
-
-    @Autowired
-    private BookmarkRepository bookmarkRepository;
+    private final BookmarkRepository bookmarkRepository;
 
     @GetMapping("/myapply")
-    public String mypage(Model model) {
+    public @ResponseBody ResponseEntity<?> mypage() {
         User principal = (User) session.getAttribute("principal");
         if (principal == null) {
             throw new CustomException("회원 인증이 되지 않았습니다. 로그인을 해주세요.", HttpStatus.UNAUTHORIZED);
         }
 
-        List<ApplyListForUserRespDto> applyList = applyRepository.findByUserId(
-                principal.getId());
-        model.addAttribute("applyLists", applyList);
-
-        List<ApplyResume> resumeList = applyResumeRepository.findByUserId(principal.getId());
-
-        model.addAttribute("resumeList", resumeList);
-
-        List<RecruitmentPostListRespDto> posts = recruitmentPostRepository.findByPost();
-        // d-day 계산
-        for (RecruitmentPostListRespDto post : posts) {
-            post.calculateDiffDays(); // D-Day 계산
+        List<UserPageApplyDto> userPageDtos = applyRepository.findAllApply(principal.getId());
+        for (UserPageApplyDto post : userPageDtos) {
+            post.getRecruitmentList().calculateDiffDays(); // D-Day 계산
         }
 
-        model.addAttribute("Posts", posts);
-
-        return "userpage/myapply";
+        return new ResponseEntity<>(new ResponseDto<>(1, "UserPage", userPageDtos), HttpStatus.OK);
     }
 
     @GetMapping("/mymatching")
-    public String mymatching(Model model) {
+    public @ResponseBody ResponseEntity<?> mymatching() throws Exception {
         User principal = (User) session.getAttribute("principal");
         if (principal == null) {
             throw new CustomException("회원 인증이 되지 않았습니다. 로그인을 해주세요.", HttpStatus.UNAUTHORIZED);
         }
-        if (principal != null) {
-            List<UserMatchingDto> userMatchingDto = userSkillRepository.userMatching(principal.getId());
-            model.addAttribute("userMatching", userMatchingDto);
+
+        List<UserPageMatchingDto> posts = userSkillRepository.userJoinRecruitmentWithMatching(principal.getId());
+
+        for (UserPageMatchingDto post : posts) {
+            List<String> skills = Convert.ete_test(post.getUserMatching().getUserSkillDto().getSkill());
+            post.getUserMatching().getUserSkillDto().setSkillString(skills);
+            post.getRecruitment().calculateDiffDays();
         }
 
-        List<RecruitmentPostListRespDto> posts = recruitmentPostRepository.findByPost();
-        // d-day 계산
-        for (RecruitmentPostListRespDto post : posts) {
-            post.calculateDiffDays(); // D-Day 계산
-        }
-
-        model.addAttribute("Posts", posts);
-
-        Map<Integer, String> skillMap = new HashMap<>();
-        skillMap.put(1, "Java");
-        skillMap.put(2, "HTML");
-        skillMap.put(3, "JavaScript");
-        skillMap.put(4, "VueJS");
-        skillMap.put(5, "CSS");
-        skillMap.put(6, "Node.js");
-        skillMap.put(7, "React");
-        skillMap.put(8, "ReactJS");
-        skillMap.put(9, "Typescript");
-        skillMap.put(10, "Zustand");
-        skillMap.put(11, "AWS");
-        model.addAttribute("skillMap", skillMap);
-        model.addAttribute("userSkillDtos", userSkillRepository.findByUserId(principal.getId()));
-
-        return "userpage/mymatching";
+        return new ResponseEntity<>(new ResponseDto<>(1, "매칭서비스 완료", posts), HttpStatus.OK);
     }
 
     @GetMapping("/mybookmark")
-    public String mybookmark(Model model, BookmarkReqDto bookmarkReqDto) {
+    public @ResponseBody ResponseEntity<?> mybookmark() {
         User principal = (User) session.getAttribute("principal");
         if (principal == null) {
             throw new CustomException("회원 인증이 되지 않았습니다. 로그인을 해주세요.", HttpStatus.UNAUTHORIZED);
         }
-
-        if (principal != null) {
-            List<BookmarkReqDto> bookmarkDto = bookmarkRepository.findByUserId(principal.getId());
-            model.addAttribute("bookmarkDto", bookmarkDto);
-        }
-
-        List<RecruitmentPostListRespDto> posts = recruitmentPostRepository.findByPost();
+        List<UserPageBookmarkDto> posts = bookmarkRepository.BookmarkJoinRecruitOfUserPage(principal.getId());
         // d-day 계산
-        for (RecruitmentPostListRespDto post : posts) {
-            post.calculateDiffDays(); // D-Day 계산
+        for (UserPageBookmarkDto post : posts) {
+            post.getRecruitmentList().calculateDiffDays(); // D-Day 계산
         }
-
-        model.addAttribute("Posts", posts);
-
-        return "userpage/mybookmark";
+        return new ResponseEntity<>(new ResponseDto<>(1, "북마크 목록", posts), HttpStatus.OK);
     }
 }
